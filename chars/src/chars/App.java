8e6c48374f44a9e95966b47216fda58dc723e36a
fmondaca <f.mondaca@uni-koeln.de>
2015-11-24 11:01:28 +0100

    App for CharChecker added

---
+++ chars/src/chars/App.java
@@ -0,0 +1,15 @@
+package chars;
+
+public class App {
+
+	public static void main(String[] args) {
+
+		CharChecker cc = new CharChecker();
+
+		String toCheck = "AAksfvj$sfv;ekekeas";
+
+		cc.iterate(toCheck);
+
+	}
+
+}
