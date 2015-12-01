9c8635f6992871ca0c6f490f5d1fe235a96d44ce
fmondaca <f.mondaca@uni-koeln.de>
2015-12-01 12:15:14 +0100

    app in charchecker - iter demo method added

--- chars/src/chars/App.java
+++ chars/src/chars/App.java
@@ -6,9 +6,34 @@ public class App {
 
 		CharChecker cc = new CharChecker();
 
-		String toCheck = "AAksfvj$sfv;ekekeas";
+		String s = "praktikum";
 
-		cc.iterate(toCheck);
+		char[] charArray = cc.toArray(s);
+		cc.printArrayFor(charArray);
+
+		// App app = new App();
+		// app.iter(s, 10);
+
+	}
+
+	void iter(String s, int limit) {
+
+		int j = 0;
+		for (; j <= limit;) {
+			System.out.println("count: " + j + " " + s);
+			j++;
+		}
+		System.out.println("\n");
+
+		System.out.println("++++++++++++++++++");
+
+		System.out.println("\n");
+
+		int i = 0;
+		while (i <= limit) {
+			System.out.println("count: " + i + " " + s);
+			i++;
+		}
 
 	}
 
